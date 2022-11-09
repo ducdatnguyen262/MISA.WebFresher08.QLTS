e7cd66e30215b41813640aeda1eb60bb6d95254b
ducdatnguyen262 <ducdatnguyen262@gmail.com>
2022-11-09 15:01:23 +0700

    update comments

--- MISA.WebFresher08.QLTS.DL/VoucherDL/VoucherDL.cs
+++ MISA.WebFresher08.QLTS.DL/VoucherDL/VoucherDL.cs
@@ -12,12 +12,12 @@ namespace MISA.WebFresher08.QLTS.DL
     public class VoucherDL : BaseDL<Voucher>, IVoucherDL
     {
         /// <summary>
-        /// Lấy danh sách các ghi tăng có chọn lọc
+        /// Lấy danh sách các chứng từ có chọn lọc
         /// </summary>
         /// <param name="keyword">Từ để tìm kiếm theo số chứng từ và nội dung</param>
-        /// <param name="limit">Số bản ghi muốn lấy</param>
+        /// <param name="limit">Số chứng từ muốn lấy</param>
         /// <param name="page">Số trang bắt đầu lấy</param>
-        /// <returns>Danh sách các ghi tăng sau khi chọn lọc và các giá trị khác</returns>
+        /// <returns>Danh sách các chứng từ sau khi chọn lọc và các giá trị khác</returns>
         /// Created by: NDDAT (08/11/2022)
         public PagingData<Voucher> FilterVouchers(string? keyword, int limit, int page)
         {
